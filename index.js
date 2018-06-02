8577684ab84b9ddbafee02e564260e3655fa8292
Edward Mixon <edwardchase@gatech.edu>
2018-06-02 00:54:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function takeANumber(katzDeliLine,newName) {
+ katzDeliLine.push(newName);
+   var pos = katzDeliLine.length;
+  return `Welcome, ${newName}. You are number ${pos} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length < 1) {
+    return 'There is nobody waiting to be served!';
+  }
+    else {
+      var temp = katzDeliLine[0];
+      katzDeliLine.shift();
+return `Currently serving ${temp}.`;
+  } 
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length < 1) {
+    return 'The line is currently empty.'
+  }
+  else {
+      for (let i = 1; i <= katzDeliLine.length; i++) {
+    katzDeliLine[i] = i, `${katzDeliLine[i]},` 
+}
+  return `The line is currently: ${katzDeliLine}`;
+}
+}
